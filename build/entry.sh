22d1349444163457421449da5fda5eda69d82efd
Quang <quang@streamkap.com>
2023-12-12 23:54:46 +0700

    update openvpn

--- build/entry.sh
+++ build/entry.sh
@@ -13,23 +13,9 @@ is_enabled() {
     [[ ${1,,} =~ ^(true|t|yes|y|1|on|enable|enabled)$ ]]
 }
 
-# Either a specific file name or a pattern.
-if [[ $CONFIG_FILE ]]; then
-    config_file=$(find /config -name "$CONFIG_FILE" 2> /dev/null | sort | shuf -n 1)
-else
-    config_file=$(find /config -name '*.conf' -o -name '*.ovpn' 2> /dev/null | sort | shuf -n 1)
-fi
-
-if [[ -z $config_file ]]; then
-    echo "no openvpn configuration file found" >&2
-    exit 1
-fi
-
-echo "using openvpn configuration file: $config_file"
-
 
 openvpn_args=(
-    "--config" "$config_file"
+    "--config" "$CONFIG_FILE"
     "--cd" "/config"
 )
 
@@ -37,10 +23,7 @@ if is_enabled "$KILL_SWITCH"; then
     openvpn_args+=("--route-up" "/usr/local/bin/killswitch.sh $ALLOWED_SUBNETS")
 fi
 
-# Docker secret that contains the credentials for accessing the VPN.
-if [[ $AUTH_SECRET ]]; then
-    openvpn_args+=("--auth-user-pass" "/run/secrets/$AUTH_SECRET")
-fi
+openvpn_args+=("--auth-user-pass" "$AUTH_SECRET")
 
 openvpn "${openvpn_args[@]}" &
 openvpn_pid=$!
